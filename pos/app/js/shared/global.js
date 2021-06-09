13d1758f06655de04c10a975bc97a5e10c4e09be
Ramil Domingo CB <ramil.domingo@connectedbusiness.com>
2021-06-09 23:34:52 +0800

    Updated to v22.13

--- pos/app/js/shared/global.js
+++ pos/app/js/shared/global.js
@@ -9,7 +9,7 @@ define(['shared/enum'],
         CB: "http://data.connectedbusiness.com/Demo_CB18/POS/WebService/"
       },
 
-      AppVersion: "22.12", //version.major + '.' + version.minor + '.' + version.build
+      AppVersion: "22.13", //version.major + '.' + version.minor
       ServerVersion: "",
       MinimumSupportedVersion: "16.0", //for backwards compatibility
       MinimumSupportedVersionForDesktop: "16.0", //for backwards compatibility
