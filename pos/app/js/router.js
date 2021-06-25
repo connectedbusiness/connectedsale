44a9444376f7590f214b272a9f99c6c9729863a8
Ramil Domingo CB <ramil.domingo@connectedbusiness.com>
2021-06-25 20:49:17 +0800

    Fixed version checker

--- pos/app/js/router.js
+++ pos/app/js/router.js
@@ -259,8 +259,9 @@ define([
 					break;
 			}
 
-			switch (currentServerVersion.Major) {
-				case "22" :
+			var majorVersion =  currentServerVersion.Major + ".0";
+			switch (majorVersion) {
+				case "22.0" :
 					_index = 11;
 					break;
 			}
